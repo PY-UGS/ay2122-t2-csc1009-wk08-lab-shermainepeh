7bbbe015a38d0312e758a9ddd8f2b6508b35669b
shermainepeh <97492858+shermainepeh@users.noreply.github.com>
2022-03-15 12:04:30 +0800

    Add files via upload

---
+++ 2101573_lab_wk08/Question2/InsufficientFundsException.java
@@ -0,0 +1,15 @@
+package Lab8.Question2;
+
+// want to write a checked exception that is automatically enforced by the Handle or Declare Rule, 
+// extend the Exception class. 
+public class InsufficientFundsException extends Exception { //considered to be checked exceptions
+    private double amount;
+
+    public InsufficientFundsException(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
